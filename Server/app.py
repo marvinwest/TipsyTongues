a7bef141831146fe5cd485e5ed705fe95a6f1a29
Marvin Westphal <marvin.westphal@stud.uni-due.de>
2021-05-11 15:36:07 +0200

    Adds initial mocked Server

--- Server/app.py
+++ Server/app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-	return "Hello World"
+# extract sentence from incoming request
+# extract the audiofile from incoming request
+# return a mocked response according to defined API
+@app.route("/recognition/audio", methods=["POST"])
+def post_recognition():
+	sentence = request.form.get("sentence")
+	audio_file = request.files.get("audioFile")
+
+	return {"levelOfDrunkenness" : 3}
+
+# Debug = True only for testpurposes
+if __name__ == "__main__":
+	print("Server started")
+	app.run(debug=True)
