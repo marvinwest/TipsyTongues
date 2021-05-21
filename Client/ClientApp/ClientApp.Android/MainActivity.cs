ba996f6f4c4da5ff203f0276ca729091873abafe
trashyjules <trashyjules@gmail.com>
2021-05-21 10:34:23 +0200

    adds initial splashpage

--- Client/ClientApp/ClientApp.Android/MainActivity.cs
+++ Client/ClientApp/ClientApp.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace ClientApp.Droid
 {
-    [Activity(Label = "ClientApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "ClientApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -25,4 +25,7 @@ namespace ClientApp.Droid
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
+
+   
 }
+   
