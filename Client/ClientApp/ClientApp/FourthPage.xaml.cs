ece42772502ec634a96573c8a00dad10b7714b82
marvinwest <marvin.westphal@stud.uni-due.de>
2021-06-25 23:01:35 +0200

    Adds Logic to display of levelOfDrunkenness

--- Client/ClientApp/ClientApp/FourthPage.xaml.cs
+++ Client/ClientApp/ClientApp/FourthPage.xaml.cs
@@ -12,33 +12,58 @@ namespace ClientApp
     
     public partial class FourthPage : ContentPage
     {
-        private int levelOfDrunkennessDisplay;
-        public int LevelOfDrunkennessDisplay
+        private String levelOfDrunkennessText;
+        public String LevelOfDrunkennessText
         {
-            get { return levelOfDrunkennessDisplay; }
-            set { levelOfDrunkennessDisplay = value;
-                OnPropertyChanged("LevelOfDrunkenness"); }
+            get { return levelOfDrunkennessText; }
+            set
+            {
+                levelOfDrunkennessText = value;
+                OnPropertyChanged("LevelOfDrunkenness");
+            }
         }
 
-        
+        private String levelOfDrunkennessImage;
+        public String LevelOfDrunkennessImage
+        {
+            get { return levelOfDrunkennessImage; }
+            set
+            {
+                levelOfDrunkennessImage = value;
+                OnPropertyChanged("LevelOfDrunkenness");
+            }
+        }
 
-        public FourthPage(int levelOfDrunkennness)
+        public FourthPage(int levelOfDrunkenness)
         {
             BindingContext = this;
-            LevelOfDrunkennessDisplay = levelOfDrunkennness;
+            BuildLevelOfDrunkennessDisplay(levelOfDrunkenness);
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
-
-
         }
-
-       
 
         private async void SecondPage_OnClicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new SecondPage());
             Navigation.RemovePage(this);
         }
+
+        private async void BuildLevelOfDrunkennessDisplay(int levelOfDrunkenness)
+        {
+            switch (levelOfDrunkenness)
+            {
+                case 0: LevelOfDrunkennessText = "sounds sober, amen"; LevelOfDrunkennessImage = "beer0.png"; break;
+                case 1: LevelOfDrunkennessText = "the night is still young"; LevelOfDrunkennessImage = "beer1.png"; break;
+                case 2: LevelOfDrunkennessText = "somebody is getting tipsy"; LevelOfDrunkennessImage = "beer2.png"; break;
+                case 3: LevelOfDrunkennessText = "sloppy drunk (absolutely hammered?)"; LevelOfDrunkennessImage = "beer3.png"; break;
+                case 4: LevelOfDrunkennessText = "you probably had one too many"; LevelOfDrunkennessImage = "beer4.png"; break;
+                default:
+                    await Navigation.PushAsync(new ErrorPage("Something wnet wrong, please try again"));
+                    Navigation.RemovePage(this);
+                    break;
+            }
+        }
+
     }
 
 }
