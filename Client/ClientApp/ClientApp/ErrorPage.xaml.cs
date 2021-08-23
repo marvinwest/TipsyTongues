b220e95deb2c3b0f37b1025a71c33b1278e085d4
marvinwest <marvin.westphal@stud.uni-due.de>
2021-08-23 18:50:17 +0200

    Uses ElementSizeService in ErrorPage

--- Client/ClientApp/ClientApp/ErrorPage.xaml.cs
+++ Client/ClientApp/ClientApp/ErrorPage.xaml.cs
@@ -12,17 +12,28 @@ namespace ClientApp
     public partial class ErrorPage : ContentPage
     {
         private String errorMessageDisplay;
-        public String ErrorMessageDisplay
-        {
-            get { return errorMessageDisplay; }
-            set { errorMessageDisplay = value;
-                OnPropertyChanged("ErrorMessageDisplay");}
-        }
+
+        private ElementSizeService elementSizeService;
+
+        private Double frameHeight;
+        private Double frameWidth;
+
+        private double menuButtonHeight;
+        private double menuButtonWidth;
 
         public ErrorPage(String errorMessage)
         {
+            errorMessageDisplay = errorMessage;
+
+            elementSizeService = new ElementSizeService();
+
+            FrameHeight = elementSizeService.calculateElementHeight(0.5);
+            FrameWidth = elementSizeService.calculateElementWidth(0.9);
+
+            menuButtonHeight = elementSizeService.calculateElementHeight(0.1);
+            menuButtonWidth = elementSizeService.calculateElementWidth(0.4);
+
             BindingContext = this;
-            ErrorMessageDisplay = errorMessage;
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
         }
@@ -32,5 +43,41 @@ namespace ClientApp
             await Navigation.PushAsync(new MainPage());
             Navigation.RemovePage(this);
         }
+
+        public String ErrorMessageDisplay
+        {
+            get { return errorMessageDisplay; }
+            set
+            {
+                errorMessageDisplay = value;
+                OnPropertyChanged("ErrorMessageDisplay");
+            }
+        }
+
+        public Double FrameHeight
+        {
+            get { return frameHeight; }
+            set { frameHeight = value; }
+        }
+
+        public Double FrameWidth
+        {
+            get { return frameWidth; }
+            set { frameWidth = value; }
+        }
+
+        public Double MenuButtonWidth
+        {
+            get { return menuButtonWidth; }
+            set { menuButtonWidth = value; }
+        }
+
+        public Double MenuButtonHeight
+        {
+            get { return menuButtonHeight; }
+            set { menuButtonHeight = value; }
+        }
+
     }
+
 }
