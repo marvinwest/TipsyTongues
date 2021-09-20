c0ff3c8e5d34f22d8984a387c679bb948c97cb38
marvinwest <marvin.westphal@stud.uni-due.de>
2021-09-20 21:03:14 +0200

    Adds comments to PermissionService

--- Client/ClientApp/ClientApp/PermissionService.cs
+++ Client/ClientApp/ClientApp/PermissionService.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Plugin.Permissions;
 using Plugin.Permissions.Abstractions;
 
 namespace ClientApp
 {
+
+    /**
+     * PermissionService:
+     * Inherits Methods to check Permissionstatus of microphone- and storagepermission.
+     * Microphone and Storage permissions are needed for the app to function.
+     **/
     class PermissionService
     {
 
+        /**
+         * Checks if the Microphone permission is given.
+         * If the permission is not given, the user is asked for the permission.
+         * Returns true if the permission is given, false if the permission is not given.
+         **/
         public static async Task<bool> checkOrGetMicrophonePermission()
         {
             var status = await CrossPermissions.Current.CheckPermissionStatusAsync<MicrophonePermission>();
@@ -31,6 +39,11 @@ namespace ClientApp
             }
         }
 
+        /**
+         * Checks if the Microphone permission is given.
+         * If the permission is not given, the user is asked for the permission.
+         * Returns true if the permission is given, false if the permission is not given.
+         **/
         public static async Task<bool> checkOrGetStoragePermission()
         {
             var status = await CrossPermissions.Current.CheckPermissionStatusAsync<StoragePermission>();
@@ -53,4 +66,5 @@ namespace ClientApp
         }
 
     }
+
 }
