c87777f95d229ad048f299cbbc8eeff7051ddd41
marvinwest <marvin.westphal@stud.uni-due.de>
2021-09-20 15:03:21 +0200

    Adds comments to pronunciation_recognizer.py

--- Server/pronunciation_recognizer.py
+++ Server/pronunciation_recognizer.py
@@ -5,18 +5,16 @@ import server_keys as keys
 
 
 # !!! DO NOT PUSH TO GITHUB WITH VALID speech_key !!!
-
-# Setup subscription info for the Speech Service
-# speech-key: your subscription key from Azure
-# service_region: The region you declared in your Azure-profile
-
+# Speech-key: Your subscription key from Azure.
+# Service_region: The region you declared in your Azure-profile.
 speech_key, service_region = keys.azure_service_key, keys.azure_service_region
 
-# only functions for short audiofiles.
-# maybe use continuous recognition instead.
+# Only functions for short audiofiles (max. 15 seconds).
+# Uses Azure Congnitive Services to do a pronunciation recognition based on languageCode, reference sentence and audiofile.
+# Returns PronunciationResult (Data transfer object, that holds the results of the recognition).
 def recognize_pronunciation(language_code, sentence, filename):
 	
-	# build necessary configs for pronunciation assesment
+	# Build necessary configs for pronunciation assesment
 	speech_config = __build_speech_config()
 	audio_config = __build_audio_config(filename)
 	language_config = __build_language_config(language_code)
@@ -26,8 +24,10 @@ def recognize_pronunciation(language_code, sentence, filename):
 	pronunciation_assessment_config = __build_pronunciation_assesment_config(sentence)
 	pronunciation_assessment_config.apply_to(speech_recognizer)
 
+	# Do the recognition.
 	response = speech_recognizer.recognize_once()
 
+	# Build PronunciationResult.
 	try:
 		result = __build_pronunciation_result(
 			speechsdk.PronunciationAssessmentResult(response))
@@ -40,21 +40,29 @@ def recognize_pronunciation(language_code, sentence, filename):
 
 	return result
 
+# Build necessary SpeechConfig from subscription information.
 def __build_speech_config():
 	return speechsdk.SpeechConfig(subscription = speech_key, region = service_region)
 
+# Build necessary AudioConfig from audiofilename.
 def __build_audio_config(audio_file_name):
 	return speechsdk.AudioConfig(filename = audio_file_name)
 
+# Build neccessary SourceLanguageConfig from languageCode.
 def __build_language_config(language_code):
 	return speechsdk.languageconfig.SourceLanguageConfig(language_code)
 
+# Builds the SpeechRecognizer from before built SpeechConfig, SourceLanguageConfig and AudioConfig.
 def __build_speech_recognizer(speech_config, language_config, audio_config):
 	return speechsdk.SpeechRecognizer(
 		speech_config = speech_config,
 		source_language_config = language_config,
 		audio_config = audio_config)
 
+# Build PronunciationAssessmentConfig from Sentence, that can then be applied to SpeechRecognizer.
+# Grading System defines that the result values are between 0 - 100.
+# Granularity Phoneme defines that the grading is based on fulltext, words and phonemes.
+# Enable miscue activates failure calculation according to reference sentence and audiofile.
 def __build_pronunciation_assesment_config(sentence):
 	return speechsdk.PronunciationAssessmentConfig(
 		reference_text = sentence,
@@ -62,9 +70,10 @@ def __build_pronunciation_assesment_config(sentence):
 		granularity = speechsdk.PronunciationAssessmentGranularity.Phoneme,
 		enable_miscue = True)
 
-def __build_pronunciation_result(pronunciation_assesment_result):
+# Build PronunciationResult from pronunciation assessment result
+def __build_pronunciation_result(pronunciation_assessment_result):
 	return PronunciationResult(
-		pronunciation_assesment_result.accuracy_score,
-		pronunciation_assesment_result.completeness_score,
-		pronunciation_assesment_result.fluency_score,
-		pronunciation_assesment_result.pronunciation_score)
+		pronunciation_assessment_result.accuracy_score,
+		pronunciation_assessment_result.completeness_score,
+		pronunciation_assessment_result.fluency_score,
+		pronunciation_assessment_result.pronunciation_score)
