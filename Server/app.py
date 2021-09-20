e8d3f68502e4416e5eadb106729c5a96aaa72906
marvinwest <marvin.westphal@stud.uni-due.de>
2021-09-20 13:55:38 +0200

    Adds comments in app.py

--- Server/app.py
+++ Server/app.py
@@ -13,12 +13,17 @@ import server_keys as keys
 
 app = Flask(__name__)
 # CORS allows other programming languages to send a request
-# that can be processed
 CORS(app)
 
-# extract sentence from incoming request
-# extract the audiofile from incoming request
-# return a mocked response according to defined API
+# Pipeline-model:
+# Extract sentence, languageCode, audioFileData and audioFile from incoming request.
+# Check if a correct authorization key was given:
+# Return mocked Response for Front-End-key, do the recognition for correct authorization-key.
+# Build the audio file from the given data and persist it to Server.
+# Do the recognition based on audiofile, reference sentence and languageCode.
+# Calculate the levelOfDrunkenness based on the recognition result.
+# Delete the temporarily persisted audiofile from the server.
+# Return the calculated levelOfDrunkenness in the response.
 @app.route("/recognition/audio", methods=["POST"])
 def post_recognition():
 	try:
@@ -41,31 +46,36 @@ def post_recognition():
 	if(authorization != keys.authorization_key):
 		return "Invalid Request", status.HTTP_400_BAD_REQUEST
 
+	# Build and persist the audiofile.
 	filename = build_audiofile(audio_file,
 		audio_channel_count,
 		audio_bytes_per_sample,
 		audio_samplerate)
 
+	# Do the recognition.
 	recognition_result = recognizer.recognize_pronunciation(language_code,
 		sentence,
 		filename)
 
+	# Calculate the levelOfDrunkenness, based on the recognition result.
 	level_of_drunkenness = calculator.calculate_drunkenness(recognition_result)
 
+	# Delete temporarily persisted audiofile from the server.
 	os.remove(filename)
 
+	# return the levelOfDrunkenness in the response.
 	return {"levelOfDrunkenness" : level_of_drunkenness}
 
-# For two or more requests at the same time, we need to randomize
-# the filename, so every file has a unique name
-# otherwise a PermissionError occurs
+# Build the audiofile
+# For two or more requests at the same time, we need to randomize the filename, so every file has a unique name.
+# Otherwise a PermissionError would occur
+# Returns the filename, by which the file can be accessed.
 def build_audiofile(audio_file, audio_channel_count, audio_bytes_per_sample, audio_samplerate):
 	filename_randomizer = random.randint(0, 9999999)
 	filename = "pronunciation_file" + "_" + str(filename_randomizer) + ".wav"
 	converter.convert_to_wave_and_save(filename, audio_file, audio_channel_count, audio_bytes_per_sample, audio_samplerate)
 	return filename
 
-# app.run(debug = True) only for testpurposes
-# Do not use it in deployment
+# Runs the application server.
 if __name__ == "__main__":
-	app.run(debug = True)
+	app.run()
